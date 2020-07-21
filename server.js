0c928c517e96be2c401eb729843972da3ceb116d
Ritesh1909 <30571983+Ritesh1909@users.noreply.github.com>
2020-07-21 20:55:06 +0530

    changing PORT for heroku

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ app.get('/profile/:id', (req, res) =>{
     .catch(err => res.status(400).json('error getting user'))
 })
 
-app.listen(3000 , ()=>{
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000 , ()=>{
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
